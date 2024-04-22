091e90f0ccbf6b2a78d35c60913e547b7a6fc83b
jas <jas@virgildoesthings.com>
2024-04-22 08:57:41 +0200

    simplified exception conditions and fixed grammar

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('-----')
-
+    print("The bot works -- this terminal will not provide further feedback unless there is an error. It's safe to detatch this window.")
 @client.event
 async def on_message(message):
     pattern = r"^!(\d*)d(\d+)$"
@@ -26,27 +26,29 @@ async def on_message(message):
     match = re.match(pattern, content)
     if match:
         size = int(match.group(2))
-        if match.group(1):
+        if match.group(1) and int(match.group(1)) > 1:
             rounds = int(match.group(1))
             await multiDice(size, rounds, message)
         else:
             await singleDice(size, message)
 
 async def singleDice(size, message):
-    user = message.author.id
-    result = random.randint(1, size)
-    await message.channel.send(f"<@{user}>, you rolled a **{result}**.")
-    print(f"A user rolled a {result} using a d{size}")
+    try:
+        user = message.author.id
+        result = random.randint(1, size)
+        await message.channel.send(f"<@{user}>, you rolled a **{result}**.")
+    except errors.HTTPException:
+        await message.channel.send("That's too much, and would probably break discord. Sorry!")
 
 async def multiDice(size, rounds, message):
-    if rounds * len(str(size)) <= 1500:
+    try:
         user = message.author.id
         rolls = []
         for x in range(rounds):
             result = random.randint(1, size)
             rolls.append(result)
         await message.channel.send(f"<@{user}>, your rolls are **{', '.join(map(str, rolls))}**.")
-    else:
+    except errors.HTTPException:
         await message.channel.send("That's too much, and would probably break discord. Sorry!")
 
 client.run(str(key))
